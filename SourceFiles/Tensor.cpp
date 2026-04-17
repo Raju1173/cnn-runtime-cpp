b406a6ff30100259870c343c23d751a4a6c6ca4d
Raju1173 <rajdeepsrivastava2009@gmail.com>
2026-04-17 09:04:38 +0530

    removed definition

--- SourceFiles/Tensor.cpp
+++ SourceFiles/Tensor.cpp
@@ -18,43 +18,6 @@ Tensor::~Tensor()
 	free(pData);
 }
 
-Tensor Conv2DForward(const Tensor& input, const Tensor& weights, const Tensor& bias)
-{
-	size_t H = input.shape[1];
-	size_t W = input.shape[2];
-
-	size_t K = weights.shape[0];
-	size_t C = weights.shape[1];
-	size_t R = weights.shape[2];
-	size_t S = weights.shape[3];
-
-	size_t H_out = H - R + 1;
-	size_t W_out = W - S + 1;
-
-	Tensor col({ C * R * S, H_out * W_out });
-
-	Im2col(input, R, S, col);
-
-	Tensor w_flat = Reshape(weights, { K, C * R * S });
-
-	Tensor out({ K, H_out * W_out });
-
-	GEMM(w_flat, col, out);
-
-	for (size_t k = 0; k < K; k++)
-	{
-		float B = bias.pData[k];
-		float* row = out.pData + k * (H_out * W_out);
-
-		for (size_t i = 0; i < H_out * W_out; i++)
-		{
-			row[i] += B;
-		}
-	}
-
-	return Reshape(out, { K, H_out, W_out });
-}
-
 void Tensor::zeros()
 {
 	for (size_t i = 0; i < numel; i++)
